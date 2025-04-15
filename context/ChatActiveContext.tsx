b7c365c226c74cfc3d0756310621cb0931ce3c98
thclough <thclough@icloud.com>
2025-04-15 00:02:37 -0400

    glowing chat button to introduce chatbot

--- context/ChatActiveContext.tsx
+++ context/ChatActiveContext.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import React, { useState, createContext, useContext, useRef } from "react";
+import React, {
+  useState,
+  createContext,
+  useContext,
+  useRef,
+  useEffect,
+} from "react";
 
 type ChatActiveContextType = {
   chatActive: boolean;
-  setChatActive: React.Dispatch<React.SetStateAction<boolean>>;
+  setChatActive: (bool: boolean) => void;
+  chatNeverActive: boolean;
   chatExpanded: boolean;
   setChatExpanded: React.Dispatch<React.SetStateAction<boolean>>;
   textAreaRef: React.RefObject<HTMLTextAreaElement>;
@@ -19,15 +26,37 @@ const ChatActiveContext = createContext<ChatActiveContextType | null>(null);
 export default function ChatActiveContextProvider({
   children,
 }: ChatActiveProviderProps) {
-  const [chatActive, setChatActive] = useState<boolean>(false);
+  const [chatActive, setChatActivePrim] = useState<boolean>(false);
   const [chatExpanded, setChatExpanded] = useState<boolean>(true);
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null); // used for expand button in mobile to not hide keyboard when clicked
+  const [chatNeverActive, setChatNeverActive] = useState<boolean>(false); // used for glowing button to hint user to use chat for the first time
+
+  const setChatActive = (bool: boolean) => {
+    setChatActivePrim(bool);
+
+    if (bool && chatNeverActive) {
+      console.log("setting chat never active to hold and change sate");
+      window.localStorage.setItem("chatNeverActive", "hold");
+      setChatNeverActive(false);
+    }
+  };
+
+  useEffect(() => {
+    // see if chat was ever active
+    const localCNA = window.localStorage.getItem("chatNeverActive") as
+      | string
+      | null;
+    if (!localCNA) {
+      setChatNeverActive(true);
+    }
+  }, []);
 
   return (
     <ChatActiveContext.Provider
       value={{
         chatActive,
         setChatActive,
+        chatNeverActive,
         chatExpanded,
         setChatExpanded,
         textAreaRef,
